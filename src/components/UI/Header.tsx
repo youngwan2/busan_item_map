2c6a6e6641e3ba0964e5df7bdedcd4eb0a98af53
KYW <qodna25@gmail.com>
2023-07-16 23:11:04 +0900

    git 초기화

---
+++ src/components/UI/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+function Header() {
+  return (
+    <header className={styles.Header}>
+      <div className={styles.search_container}>
+        <label className={styles.search_icon} htmlFor="search">
+          <FontAwesomeIcon style={{color:'white'}} icon={faMagnifyingGlass} />
+        </label>
+        <input placeholder="부산시 구군/물품명을 입력해보세요!" type="text" id="search" className={styles.search_input} />
+      </div>
+      <nav></nav>
+    </header>
+  );
+}
+
+export default Header;
