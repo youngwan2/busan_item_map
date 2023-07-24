5374aac7da05582c37fcadfc29e6ec1fc49743da
KYW <qodna25@gmail.com>
2023-07-24 17:46:42 +0900

    Select.tsx:: itemType에 관련 설명 주석추가

--- src/components/page/Select.tsx
+++ src/components/page/Select.tsx
@@ -7,26 +7,27 @@ interface ItemType {
     ANIMAL_PLANT: string,
     BGN_YEAR: string,
     DESC_KOR: string,
-    NUTR_CONT1: string,
-    NUTR_CONT2: string,
-    NUTR_CONT3: string,
-    NUTR_CONT4: string,
-    NUTR_CONT5: string,
-    NUTR_CONT6: string,
-    NUTR_CONT7: string,
-    NUTR_CONT8: string,
-    NUTR_CONT9: string,
-    SERVING_WTL: string
+    NUTR_CONT1: string, // 열량(kcal)
+    NUTR_CONT2: string, // 탄수화물(g)
+    NUTR_CONT3: string, // 단백질(g)
+    NUTR_CONT4: string, // 지방(g)
+    NUTR_CONT5: string, // 당류(g)
+    NUTR_CONT6: string, // 나트륨(mg)
+    NUTR_CONT7: string, // 콜레스테롤(mg)
+    NUTR_CONT8: string, // 포화지방산(g)
+    NUTR_CONT9: string, // 트랜스지방산(g)
+    SERVING_WT: string // 1회 제공량(g)
 }
+
+
 function Select() {
 
-    const [topFixed, setTopFixed] =useState(true)
-
+    const [topFixed, setTopFixed] = useState(true)
 
     return (
         <section className={styles.select}>
             <article className={styles.select_con}>
-                <Search fixed ={topFixed}></Search>
+                <Search fixed={topFixed}></Search>
             </article>
 
             <h2>조회된 데이터 목록은 여기에</h2>
