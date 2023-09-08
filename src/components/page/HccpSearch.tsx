a376e18800006ae1b8dbaa7be3561d2e13f3e933
KYW <qodna25@gmail.com>
2023-09-08 23:56:29 +0900

    이미지파일없을 때 대체 이미지 수정

--- src/components/page/HccpSearch.tsx
+++ src/components/page/HccpSearch.tsx
@@ -70,6 +70,7 @@ function HccpSearch() {
     <div className={styles.Haccp}>
       <Header isStyle={true} />
 
+      {/* 검색창 */}
       <div className={styles.search_container}>
         <input
           ref={input}
@@ -100,7 +101,9 @@ function HccpSearch() {
           <ReactSpinner />
         </div>
       </div>
+      {/* 검색 결과 보이는 곳 */}
       <section className={styles.content_container}>
+        <p className={styles.message}> <span>잠깐 알고가기</span> <br />해썹(HACCP) 제도는 식품, 축산물, 사료 등을 만드는 과정에서 생물학적, 화학적, 물리적 위해요인들이 발생할 수 있는 상황을 과학적으로 분석하고 사전에 위해요인의 발생여건들을 차단하여 소비자에게 안전하고 깨끗한 제품을 공급하기 위한 시스템적인 규정을 말한다.</p> <br />
         {Array.isArray(items) ? (
           items.map((item) => {
             return (
--- src/components/page/LocalFood.tsx
+++ src/components/page/LocalFood.tsx
@@ -33,7 +33,7 @@ const LocalFood = () => {
     const categoryFilter = () => {
 
         const filterList: any[] = []
-        localFoods?.forEach((data, i) => {
+        localFoods?.forEach((data) => {
             filterList.push(data.data_title_nm)
         })
         const filterTitle = [...new Set(filterList)]
@@ -50,7 +50,8 @@ const LocalFood = () => {
     const searchFilter = (e: MouseEvent<HTMLLIElement>) => {
         const choiceTitle = e.currentTarget.textContent!
 
-        const choiceFood = localFoods.filter((food, i) => {
+        // 제목 리스트에서 유저가 선택한 항목이 포함되어 있는 경우만 필터링
+        const choiceFood = localFoods.filter((food) => {
             return (food.data_title_nm.includes(choiceTitle))
         })
         setSearchResult(choiceFood)
@@ -64,8 +65,9 @@ const LocalFood = () => {
         <>
             <Header isStyle={true} />
 
-            <article className={styles.category_con}>
-                <ol className={styles.category} style={display ? { display: 'block' } : { display: 'none' }}>
+            <article className={styles.category_con} style={display ? { display: 'block' } : { display: 'none' }}>
+                <input className={styles.category_search_input} type="text" placeholder='fsdfds'/>
+                <ol className={styles.category} >
                     <button
                         onClick={() => {
                             setDisplay(false)
@@ -90,7 +92,7 @@ const LocalFood = () => {
                     <p>※ 현재 기능은 검색 기능없이 카테고리 내 주제별 선택을 통해 검색이 가능합니다.</p>
 
                     {/* 대표 이미지 */}
-                    <img width={380} height={380} src={searchResult !== undefined ? `${searchResult[0].main_thumb_url}` : process.env.PUBLIC_URL + '/error.png'} alt='thumb'></img>
+                    <img width={380} height={380} src={searchResult !== undefined && searchResult[0].main_thumb_url !=='' ? `${searchResult[0].main_thumb_url}` : process.env.PUBLIC_URL + '/not-image.png'} alt='thumb'></img>
                     <h3 className={styles.sub_title}>{searchResult !== undefined ? searchResult[0].sbjt_nm : null}</h3>
                     <p className={styles.foods_sumry}> {searchResult !== undefined ? searchResult[0].sumry_cn.replace(/&nbsp;/gi, ' ') : null}</p>
                     <p className={styles.foods_detail_url}>{searchResult !== undefined ? searchResult[0].cntnts_url : null}</p>
@@ -98,6 +100,7 @@ const LocalFood = () => {
                         {searchResult?.map((foods) => {
                             return (
                                 <div className={styles.foods}>
+                                    <p><span>식별 </span>{foods.data_manage_no}</p>
                                     <p><span>주소 </span>{foods.addr}</p>
                                     <p><span>관련 식당명 </span>{foods.relate_rstrnt_nm}</p>
                                     <p><span>식당번호 </span>{foods.rstrnt_tel_no}</p>
