c71a29bec6e5fb75af42fe3adb968c3f6f239f3a
KYW <qodna25@gmail.com>
2023-10-27 11:49:19 +0900

    헤더 메뉴 ui 수정:: 정보조회 메뉴를 조회서비스로 통합하고,   드롭다운 형식으로 수정

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -21,80 +21,85 @@ function Header({ isStyle }: HeaderType) {
   });
   return (
     <>
-    <div
-      className="header_scroll"
-      style={
-        scroll > 50
-          ? { visibility: "hidden", transform: `translateY(-100px)` }
-          : { visibility: "visible", transform: `translateY(-8px)` }
-      }
-    >
-      <header
-        className={styles.header}
+      <div
+        className="header_scroll"
         style={
-          isStyle
-            ? { backgroundColor: "rgba(61, 61, 255, 0.794)" }
-            : { backgroundColor: " rgba(0, 0, 0, 0.528)" }
+          scroll > 50
+            ? { visibility: "hidden", transform: `translateY(-100px)` }
+            : { visibility: "visible", transform: `translateY(-8px)` }
         }
       >
-        <div className={styles.header_flex}>
-          <h1>
-            <span
-              className={styles.home_log}
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              FoodPicker
-            </span>
-          </h1>
-          <nav className={onDisplay ? styles.onMenu : styles.offMenu}>
-            <button
-              className={styles.closeBtn}
-              style={!onDisplay ? { display: "none" } : { display: "block" }}
-              onClick={menuDisplayFun}
-            >
-              X
-            </button>
-            <ul>
-              <li>
-                <Link to={"/localfood"}>향토음식조회</Link>
-              </li>
-              <li>
-                <Link to={"/nutrition"}>식품영양정보조회</Link>
-              </li>
+        <header
+          className={styles.header}
+          style={
+            isStyle
+              ? { backgroundColor: "rgba(61, 61, 255, 0.794)" }
+              : { backgroundColor: " rgba(0, 0, 0, 0.528)" }
+          }
+        >
+          <div className={styles.header_flex}>
+            <h1>
+              <span
+                className={styles.home_log}
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                FoodPicker
+              </span>
+            </h1>
+            <nav className={onDisplay ? styles.onMenu : styles.offMenu}>
+              <button
+                className={styles.closeBtn}
+                style={!onDisplay ? { display: "none" } : { display: "block" }}
+                onClick={menuDisplayFun}
+              >
+                X
+              </button>
+              <ul>
+                <li className={styles.main_menu_con}>
+                  <Link to={"#"}>조회 서비스</Link>
+                  <ul className={styles.main_menu_ul}>
+                    <li>
+                      <Link to={"/localfood"}>향토음식조회</Link>
+                    </li>
+                    <li>
+                      <Link to={"/nutrition"}>식품영양정보조회</Link>
+                    </li>
 
-              <li>
-                <Link to={"/haccp"}>HACCP제품정보조회</Link>
-              </li>
-              <li>
-                <Link to={"/food-recipe"}>레시피정보조회</Link>
-              </li>
-              <li className={styles.etc_menu_con}>
-              <Link to={"#"}>기타 서비스</Link>
-                <ul className={styles.etc_menu}>
-                  <li>
-                    <Link to={"/item"}>부산생필품정보</Link>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
-          {/* 가로 624 px 이하 부터 적용 */}
-          <button
-            className={styles.menu_icon}
-            onClick={menuDisplayFun}
-            style={
-              onDisplay ? { visibility: "hidden" } : { visibility: "visible" }
-            }
-          >
-            <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
-          </button>
-        </div>
-      </header>
-      
-    </div>
-    <Outlet/>
+                    <li>
+                      <Link to={"/haccp"}>HACCP제품정보조회</Link>
+                    </li>
+                    <li>
+                      <Link to={"/food-recipe"}>레시피정보조회</Link>
+                    </li>
+                  </ul>
+                </li>
+
+                <li className={styles.etc_menu_con}>
+                  <Link to={"#"}>기타 서비스</Link>
+                  <ul className={styles.etc_menu}>
+                    <li>
+                      <Link to={"/item"}>부산생필품정보</Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+            {/* 가로 624 px 이하 부터 적용 */}
+            <button
+              className={styles.menu_icon}
+              onClick={menuDisplayFun}
+              style={
+                onDisplay ? { visibility: "hidden" } : { visibility: "visible" }
+              }
+            >
+              <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+            </button>
+          </div>
+        </header>
+      </div>
+      <Outlet />
     </>
   );
 }
