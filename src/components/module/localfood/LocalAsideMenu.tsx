09729d267c6bf54ec456abde61cecc7523f1831a
KYW <qodna25@gmail.com>
2023-11-01 10:28:10 +0900

    향토음식 조회 기능별 컴포넌트 분리

---
+++ src/components/module/localfood/LocalAsideMenu.tsx
@@ -0,0 +1,138 @@
+import { useRef, useEffect,useState, MouseEvent, KeyboardEvent } from "react";
+import { localFoods } from "../../page/localfood(202212)";
+import styles from "../../page/LocalFood.module.css";
+import { localFoodType } from "../../page/LocalFood";
+
+interface PropsType {
+  setSearchResult: (p: localFoodType[]) => void;
+  display: boolean;
+  setDisplay: (p: boolean) => void;
+}
+
+function LocalAsideMenu({ setSearchResult,display,setDisplay }: PropsType) {
+  const categoryRef = useRef<HTMLOListElement>(null);
+  const topBtn = useRef<HTMLLIElement>(null);
+  const bottomBtn = useRef<HTMLLIElement>(null);
+
+  const [title, setTitle] = useState<string[]>();
+  const [searchTargetTitle, setSearchTargetTitle] = useState("");
+  // 카테고리 상단 이동
+  const categoryTopShifter = () => {
+    topBtn.current?.addEventListener("click", () => {
+      categoryRef.current?.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  };
+
+  // 카테고리 하단 이동
+  const catgoryBottomShiter = () => {
+    bottomBtn.current?.addEventListener("click", () => {
+      categoryRef.current?.scrollTo({ top: 100000, behavior: "smooth" });
+    });
+  };
+
+  // 카테고리 중 목록 선택시 데이터 조회하는 함수
+  const searchFilter = (e: MouseEvent<HTMLLIElement>) => {
+    const choiceTitle = e.currentTarget.textContent!;
+
+    // 제목 리스트에서 유저가 선택한 항목이 포함되어 있는 경우만 필터링
+    const choiceFood = localFoods.filter((food) => {
+      return food.data_title_nm.includes(choiceTitle);
+    });
+    setSearchResult(choiceFood);
+  };
+
+  // 주제별 필터 검색 함수
+  const titleFilter = (e: KeyboardEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    setSearchTargetTitle(value); // 검색 버튼 클릭 시 조회에 활용 예정
+
+    if (e.key === "Enter") {
+      setTitle(categorySearchFilter(value));
+    }
+    if (value.length < 1) {
+      categoryFilter();
+    }
+  };
+
+  // 전체 목록에서 중복을 제외한 카테고리 목록을 필터링하는 함수
+  const categoryFilter = () => {
+    const filterList: any[] = [];
+    localFoods?.forEach((data) => {
+      filterList.push(data.data_title_nm);
+    });
+    const filterTitle = [...new Set(filterList)];
+    setTitle(filterTitle);
+  };
+
+  // 카테고리 검색 함수
+  const categorySearch = (e: MouseEvent<HTMLButtonElement>) => {
+    const searchList = categorySearchFilter(searchTargetTitle);
+    setTitle(searchList);
+  };
+
+  // 카테고리 검색어와 일치하는 문자열 배열만 반환하는 함수(필터링함수)
+  function categorySearchFilter(value: string) {
+    const filterTitle = title?.filter((title) => {
+      return title.includes(value);
+    });
+
+    return filterTitle;
+  }
+  useEffect(() => {
+    categoryFilter();
+  }, []);
+
+  return (
+    <aside
+      className={styles.category_con}
+      style={display ? { display: "block" } : { display: "none" }}
+    >
+      <input
+        onKeyUp={titleFilter}
+        className={styles.category_search_input}
+        type="text"
+        placeholder="키워드를 입력하세요!"
+      />
+      <button className={styles.search_btn} onClick={categorySearch}>
+        <img
+          src={process.env.PUBLIC_URL + `/icon/search.svg`}
+          alt="searchIcon"
+          width={30}
+          height={35}
+        />
+      </button>
+      <ol className={styles.category} ref={categoryRef}>
+        <button
+          onClick={() => {
+            setDisplay(false);
+          }}
+          className={styles.slide_btn_inner}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "/icon/close.svg"}
+            width={30}
+            height={30}
+            alt=""
+          />
+        </button>
+        {title?.map((title, i) => {
+          return (
+            <li key={i} onClick={searchFilter}>
+              {title}
+            </li>
+          );
+        })}
+      </ol>
+      <ul className={styles.category_shift_btn_con}>
+        <li onClick={categoryTopShifter} ref={topBtn}>
+          ▲
+        </li>
+        <li onClick={catgoryBottomShiter} ref={bottomBtn}>
+          ▼
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+export default LocalAsideMenu;
--- src/components/module/localfood/LocalFoodContent.tsx
+++ src/components/module/localfood/LocalFoodContent.tsx
@@ -1,5 +1,5 @@
-import { localFoodType } from "../page/LocalFood";
-import styles from "../page/LocalFood.module.css";
+import { localFoodType } from "../../page/LocalFood";
+import styles from "../../page/LocalFood.module.css";
 import LocalSearchResult from "./LocalSearchResult";
 
 interface PropsType {
@@ -18,16 +18,17 @@ function LocalFoodContent({ setDisplay, display, searchResult }: PropsType) {
         className={styles.slide_btn_outer}
         style={!display ? { display: "block" } : { display: "none" }}
       >
-        카테고리
+        메뉴
       </button>
 
       {/* 주제별 세부 내용 */}
       <h2 className={styles.title}>향토음식조회</h2>
       <section className={styles.contents}>
-        <p>
-          ※ 현재 기능은 검색 기능없이 카테고리 내 주제별 선택을 통해 검색이
-          가능합니다.
-        </p>
+        <h3 className={styles.sub_title}>
+          {searchResult !== undefined
+            ? searchResult[0].data_title_nm
+            : "우리 지역의 향토 음식을 찾아보자!"}
+        </h3>
 
         {/* 대표 이미지 */}
         <img
@@ -39,9 +40,7 @@ function LocalFoodContent({ setDisplay, display, searchResult }: PropsType) {
           }
           alt="thumb"
         ></img>
-        <h3 className={styles.sub_title}>
-          {searchResult !== undefined ? searchResult[0].data_title_nm : null}
-        </h3>
+
         <p className={styles.foods_sumry}>
           {" "}
           {searchResult !== undefined
@@ -63,7 +62,9 @@ function LocalFoodContent({ setDisplay, display, searchResult }: PropsType) {
               : '"향토음식"이란 지방의 특산품이나 특유의 조리법 등을 이용하여 만든 그 지역의 전통 음식을 의미합니다(링크). '}
           </a>
         </p>
-        <h2 style={{ marginTop: "3rem" }}>관련 식당</h2>
+        <h2 style={{ marginTop: "3rem" }}>
+          {searchResult?.length! > 0 ? "관련식당" : null}
+        </h2>
         <LocalSearchResult searchResult={searchResult}></LocalSearchResult>
       </section>
     </section>
