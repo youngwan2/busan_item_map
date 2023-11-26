a127e0f53a45f33e8e7e1f4214e9637b9b0890b7
Kim Young Wan <qodna25@gmail.com>
2023-11-26 11:49:02 +0900

    fix: 페이지네이션 기능 오류 및 영양정보조회 페이지 잘못된 경로 렌더링 문제 수정(#6, #7)
    커밋의 주요 이슈:
    1. 페이지네이션 기능 오류 수정:
    - 페이지네이션 컴포넌트의 로직을 수정하여 마지막 페이지가 정확하게 계산되록 조정했습니다.
    - 마지막 페이지가 전체 페이지 보다 크거나 같을 때 페이지 버튼이 올바르게 렌더링되도록 조치했습니다.
    2. 영양정보조회 페이지 렌더링 문제 수정:
    - 영양정보조회 페이지에서 잘못된 경로로 렌더링되는 문제를 식별했고, 수정했습니다.
    - 올바른 경로를 사용하여 서버 측 결과 화면이 아닌 클라이언트 측 라우터 경로로 렌더링되도록 조치하였습니다.
    
    이슈 번호: #6, #7

--- src/pages/Nutrition/components/NutritionPagination.tsx
+++ src/pages/Nutrition/components/NutritionPagination.tsx
@@ -4,48 +4,63 @@ import { NutritionPageNumber } from "../../../atom/NutritionsAtom"
 import { useRecoilState } from "recoil"
 
 interface PropsType {
-    totalPage:number
+    totalPage: number
 }
-const NutritionPagination = ({totalPage}:PropsType) => {
+const NutritionPagination = ({ totalPage }: PropsType) => {
     // eslint-disable-next-line prefer-const
-    let [page,setPage] = useRecoilState(NutritionPageNumber)
-    const pageBtnCount = useRef<number>(5)
+    let [page, setPage] = useRecoilState(NutritionPageNumber)
+    const pageSize = useRef<number>(5)
     const [liElements, setLiElements] = useState<JSX.Element[]>()
-    const [pageGroup, setPageGroup] = useState(1)
-    const [lastPage, setLastPage] = useState(pageGroup * pageBtnCount.current)
-    const [firstPage, setFirstPage] = useState(lastPage - pageBtnCount.current + 1)
+    const [pageGroup, setPageGroup] = useState(0)
+    const [lastPage, setLastPage] = useState(0)
+    const [firstPage, setFirstPage] = useState(0)
 
+    const isLastPageGreaterThanTotal = lastPage>=totalPage
+    const isFirstPageGreaterThanCurrentPage = firstPage>page
     const elementRendererFun = useCallback((page: number, pageGroup: number) => {
-        setPageGroup(Math.ceil(page/ pageBtnCount.current))
-        setLastPage(pageGroup*pageBtnCount.current)
-        setFirstPage(lastPage - pageBtnCount.current + 1)
-
+        setPageGroup(Math.ceil(page / pageSize.current))
+        setLastPage(pageGroup * pageSize.current)
         const lis: JSX.Element[] = []
-        if(lastPage>=totalPage) {setLastPage(totalPage)}
-        for (let i = firstPage ; i <= lastPage; i++) {
+        
+        if (isLastPageGreaterThanTotal) { setLastPage(totalPage)
+        }
+        if(isLastPageGreaterThanTotal && isFirstPageGreaterThanCurrentPage ){
+            setLastPage(pageGroup * pageSize.current)
+        }
+        for (let i = firstPage; i <= lastPage; i++) {
             lis.push(
                 <li
                     onClick={(e) => {
                         const pageNumber = Number(e.currentTarget.textContent)
                         setPage(pageNumber)
-                        setPageGroup(page / pageBtnCount.current)
+                        setPageGroup(page / pageSize.current)
 
                     }}
                     style={page === i ? { boxShadow: 'inset 50px 50px 0 white', color: 'black', border: '1px solid gray' } : { boxShadow: 'inset 50px 50px 0 0 rgb(131, 131, 240)' }}>
                     {i}
                 </li>)
         }
+
         setLiElements(lis)
 
-    }, [setPage,firstPage,lastPage,totalPage])
+    }, [setPage, firstPage, lastPage, totalPage,isFirstPageGreaterThanCurrentPage,isLastPageGreaterThanTotal])
 
+    // 마지막 페이지가 짝수일 때 보여지는 페이지 버튼 수를 조정하는 이펙트
+    useEffect(()=>{
+        if(lastPage === totalPage) {
+            setFirstPage(lastPage - pageSize.current+2)
+        } else {
+            setFirstPage(lastPage - pageSize.current + 1)
+        }
+    },[firstPage,lastPage,totalPage])
 
+   // 페이지네이션 초깃값을 셋팅하고, 변동 사항을 추적하여 반영하는 이펙트
     useEffect(() => {
         elementRendererFun(page, pageGroup)
     }, [page, pageGroup, elementRendererFun])
 
     return (
-        <ul className={styles.pagination_container} style={totalPage===0?{display:'none'}:{display:'flex'}}>
+        <ul className={styles.pagination_container} style={totalPage === 0 ? { display: 'none' } : { display: 'flex' }}>
             <li
                 style={page <= 1 ? { visibility: 'hidden', opacity: 0 } : { visibility: 'visible', opacity: 1 }}
                 onClick={() => {
@@ -55,28 +70,14 @@ const NutritionPagination = ({totalPage}:PropsType) => {
                 const id = li.props.children
                 return <Fragment key={id}>{li}</Fragment>
             })}
-            <li 
-            style={lastPage===totalPage?{visibility:'hidden',opacity:0}:{visibility:'visible',opacity:1}}
-            onClick={() => {
-                setPage(++page)
-            }}>next</li>
+            <li
+                style={totalPage === page ? { visibility: 'hidden', opacity: 0 } : { visibility: 'visible', opacity: 1 }}
+                onClick={() => {
+                    setPage(Math.min(++page, totalPage))
+                }}>next</li>
         </ul>
     )
 }
 
 
 export default NutritionPagination
-
-
-// 현재페이지
-// const currentPage = page
-// 페이지네이션 버튼의 수
-// const pageButtonCount = 5
-// 현재페이지 그룹
-// const group = Math.ceil(1/pageButtonCount)
-// 마지막 페이지
-// const lastPage =pageGroup*pageButtonCount
-
-// 첫 번째 페이지
-// const firstPage = lastPage -(pageButtonCount+1)
-
