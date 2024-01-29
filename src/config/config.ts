6a6d2487942156358dbe86080d543463275cf1e6
Kim Young Wan <qodna25@gmail.com>
2024-01-29 23:00:01 +0900

    --불가피한 임시커밋 분기점--

---
+++ src/config/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    prefix: import.meta.env.VITE_DOMAIN_PREFIX,
+    host: import.meta.env.VITE_DOMAIN_HOST
+}
---
+++ src/pages/LocalFood/components/LocalFoodList.tsx
@@ -0,0 +1,16 @@
+
+
+
+const LocalFoodList=()=>{
+
+    return (
+        <ul>
+            <h2>음식리스트</h2>
+        </ul>
+    )
+}
+
+
+
+export default LocalFoodList
+
