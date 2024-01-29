f7231ed0d04809d8f655b50613d64d7a74544f6d
KYW <qodna25@gmail.com>
2024-01-29 23:39:28 +0900

    fix/원격저장소와 노트북 로컬 저장소 충돌문제 해결

--- src/pages/Diet/ChildDietPage.tsx
+++ src/pages/Diet/ChildDietPage.tsx
@@ -1,16 +1,18 @@
 import axios from "axios";
 import styles from './ChildDiet.module.scss'
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { DietType } from "./types/Diet.types";
 import ChildDietResults from "./components/ChildDietResults";
 import ChildDietFilterMenu from "./components/ChildDietFIlterMenu";
+import { SplitText } from "text-split-simple-js";
+import gsap from "gsap";
 
 const ChildDietPage = () => {
-
+    const h1Ref = useRef<HTMLHeadingElement>(null)
     const [childDietList, setChildDietList] = useState<DietType[]>([])
     const [choiceKeyword, setChoiceKeyword] = useState('')
-    const [choiceFoodIngredient,setChoiceFoodIngredient] = useState('')
-    const [totalItemCount,setTotalItemCount] = useState(0)
+    const [choiceFoodIngredient, setChoiceFoodIngredient] = useState('')
+    const [totalItemCount, setTotalItemCount] = useState(0)
 
     async function axiosReqByEnvMode(url: string) {
         const response = await axios.get(url)
@@ -19,7 +21,20 @@ const ChildDietPage = () => {
         setTotalItemCount(data.length)
     }
 
-    const requestChildDietDataFromDB = useCallback((choiceKeyword:string, choiceFoodIngredient:string) => {
+    useEffect(() => {
+        if (h1Ref.current instanceof HTMLHeadingElement) {
+            const id = "#" + h1Ref.current.id
+            const chars = new SplitText(id).chars()
+            gsap.from(chars, {
+                y: 150, stagger: {
+                    from: 'center',
+                    amount: 1
+                }
+            })
+        }
+    }, [])
+
+    const requestChildDietDataFromDB = useCallback((choiceKeyword: string, choiceFoodIngredient: string) => {
         try {
             if (import.meta.env.MODE !== 'production') {
                 axiosReqByEnvMode(`http://localhost:3000/diet/child-diet?keyword=${choiceKeyword}&ingredient=${choiceFoodIngredient}`)
@@ -34,15 +49,18 @@ const ChildDietPage = () => {
 
     useEffect(() => {
         requestChildDietDataFromDB(choiceKeyword, choiceFoodIngredient)
-    }, [requestChildDietDataFromDB,choiceFoodIngredient,choiceKeyword])
+    }, [requestChildDietDataFromDB, choiceFoodIngredient, choiceKeyword])
 
     return (
         <section className={styles.Diet_section}>
+
             <h2 className={styles.page_title}> <p>유아 식단</p></h2>
             <div className={styles.diet_body_area}>
-            <ChildDietFilterMenu setChoiceKeyword={setChoiceKeyword} setChoiceFoodIngredient={setChoiceFoodIngredient} totalItemCount={totalItemCount} />
-            <ChildDietResults childDietList={childDietList} />
+                <ChildDietFilterMenu setChoiceKeyword={setChoiceKeyword} setChoiceFoodIngredient={setChoiceFoodIngredient} totalItemCount={totalItemCount} />
+                <ChildDietResults childDietList={childDietList} />
+                <h1 ref={h1Ref} id="diet_title">안녕하세요 제목입니다.</h1>
             </div>
+
         </section>
     );
 }
