60979dd8305db03fa7cb68e05b8b11395ae8aef2
Kim Young Wan <qodna25@gmail.com>
2024-05-28 12:41:28 +0900

    feat:(#17) 브라우저 스토로지 기반의 캐싱처리 기능 구현(#17)

--- src/pages/Recipe/RecipeDetail.tsx
+++ src/pages/Recipe/RecipeDetail.tsx
@@ -10,7 +10,8 @@ import GuideMessage from '../../components/Common/GuideMessage';
 import ReactSpinner from '../../components/UI/ReactSpinner';
 import RecipeContents from './components/RecipeContents';
 
-import { RecipeType } from '@/types/Recipe.types';
+import { RecipeInfoType, RecipeType } from '@/types/Recipe.types';
+import { StorageType, getStoreage } from '@/utils/storage';
 
 function RecipeDetail() {
   const detaildivRef = useRef<HTMLBaseElement>(null);
@@ -21,6 +22,23 @@ function RecipeDetail() {
   useEffect(() => {
     document.title = '레시피 상세조회 | FoodPicker';
   }, []);
+
+
+  /** 사용자가 선택한 레시피 반환 */
+  function extractRecipe(recipes: RecipeType[], recipe_id: string) {
+    return recipes.filter(recipe => recipe.RCP_SEQ === recipe_id)[0]
+  }
+
+  /** 레시피 업데이트 */
+  function updateRecipeData(recipe_id: string = '0') {
+    const recipeInfo: RecipeInfoType = getStoreage(StorageType.SESSION, 'recipes')
+    const recipe = extractRecipe(recipeInfo.recipes, recipe_id)
+    setRecipe(recipe)
+
+  }
+  useEffect(() => {
+    updateRecipeData(params.id)
+  }, [params.id])
 
 
   useEffect(() => {
@@ -35,7 +53,7 @@ function RecipeDetail() {
     <section className={styles.recipe_detail} ref={detaildivRef}>
       <BackMove />
       <GuideMessage path='/recipe' mainName='조회서비스' subName='음식레시피' finalPathName={recipe.RCP_NM} />
-      
+
       <RecipeContents recipe={recipe} />
       <RecipeNutrition recipe={recipe} />
       <NextRecipe param={params.id} />
--- src/pages/Recipe/components/RecipeButton.tsx
+++ src/pages/Recipe/components/RecipeButton.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from 'react';
 import styles from '../RecipeDetail.module.scss';
 import { Link } from 'react-router-dom';
-import { useAppSelector } from '../../../app/hooks';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 import useIntersection from '../../../hooks/useIntersection';
 
@@ -9,15 +8,6 @@ interface PropsType {
   param?: string;
 }
 function NextRecipe({ param }: PropsType) {
-  const state = useAppSelector((state) => {
-    return state.recipe.value;
-  });
-  const currentIndex = state.findIndex((recipe) => {
-    return recipe.RCP_SEQ === param;
-  });
-  const lastIndex = state.length - 1;
-  const prevRecipe = state[currentIndex - 1];
-  const nextRecipe = state[currentIndex + 1];
 
   const articleRef = useRef<HTMLAreaElement>(null);
 
@@ -49,7 +39,7 @@ function NextRecipe({ param }: PropsType) {
 
   return (
     <article className={styles.pagination_article} ref={articleRef}>
-      <button
+      {/* <button
         style={
           currentIndex <= 0
             ? { visibility: 'hidden', opacity: 0 }
@@ -81,7 +71,7 @@ function NextRecipe({ param }: PropsType) {
             <FaArrowAltCircleRight />
           </span>
         </Link>
-      </button>
+      </button> */}
     </article>
   );
 }
